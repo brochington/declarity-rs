d530a5f51a8a3f0366a7de492b0b4c37577f9b18
Broch Stilley <brochington@gmail.com>
2019-02-09 20:35:53 -0800

    Re-init the repo to start over.

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cargo +nightly build --target wasm32-unknown-unknown --release
-wasm-gc target/wasm32-unknown-unknown/release/declarity_rs.wasm -o server/static/declarity_rs.wasm
--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-extern crate declarity;
-
-#[no_mangle]
-pub extern fn add_one(a: u32) -> u32 {
-  a + 1
-}
-
